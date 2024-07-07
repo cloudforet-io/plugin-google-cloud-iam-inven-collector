3d608232d9c452eab106fc3c04df01ab586d3349
Jongmin Kim <whdalsrnt@megazone.com>
2024-07-07 21:17:56 +0900

    feat: implement group resource
    Signed-off-by: Jongmin Kim <whdalsrnt@megazone.com>

--- src/plugin/connector/cloud_identity_connector.py
+++ src/plugin/connector/cloud_identity_connector.py
@@ -13,9 +13,30 @@ class CloudIdentityConnector(GoogleCloudConnector):
     def list_groups(self, customer_id):
         parent = f"customers/{customer_id}"
         result = self.client.groups().list(parent=parent).execute()
-        print(result)
         return result.get("groups", [])
 
+    def get_group(self, name):
+        result = self.client.groups().get(name=name).execute()
+        return result
+
     def list_memberships(self, parent):
-        result = self.client.groups().memberships(parent=parent).list().execute()
-        return result.get("memberships", [])
+        memberships = []
+        request = self.client.groups().memberships().list(parent=parent)
+
+        while True:
+            response = request.execute()
+
+            memberships.extend(response.get("memberships", []))
+
+            request = (
+                self.client.groups().memberships()
+                .list_next(previous_request=request, previous_response=response)
+            )
+
+            if request is None:
+                break
+
+        return memberships
+
+    def get_membership(self, name):
+        return self.client.groups().memberships().get(name=name).execute()
--- src/plugin/manager/iam/group_manager.py
+++ src/plugin/manager/iam/group_manager.py
@@ -27,8 +27,55 @@ class GroupManager(ResourceManager):
     def collect_cloud_services(self, options: dict, secret_data: dict, schema: str) -> Generator[dict, None, None]:
         self.identity_connector = CloudIdentityConnector(options, secret_data, schema)
         self.rm_v3_connector = ResourceManagerV3Connector(options, secret_data, schema)
+
         for organization in self.rm_v3_connector.search_organizations():
-            print(organization)
-            customer_id = organization.get('directoryCustomerId')
-            groups = self.identity_connector.list_groups(customer_id)
-            print(groups)
+            yield from self.collect_groups(organization)
+
+    def collect_groups(self, organization: dict) -> Generator[dict, None, None]:
+        customer_id = organization.get('directoryCustomerId')
+        organization_id = organization.get('name')
+        organization_name = organization.get("displayName")
+        groups = self.identity_connector.list_groups(customer_id)
+        for group in groups:
+            yield self.make_group_info(group, organization_id, organization_name)
+
+    def make_group_info(self, group: dict, organization_id: str, organization_name: str) -> dict:
+        group_id = group.get("name")
+        name = group.get("displayName")
+        organization_id = organization_id.split("/")[-1]
+
+        group["members"] = self.get_group_members(group_id)
+        group["memberCount"] = len(group["members"])
+
+        return make_cloud_service(
+            name=name,
+            cloud_service_type=self.cloud_service_type,
+            cloud_service_group=self.cloud_service_group,
+            provider=self.provider,
+            account=f"organizations/{organization_name}",
+            data=group,
+            region_code="global",
+            reference={
+                "resource_id": group_id,
+                "external_link": f"https://console.cloud.google.com/iam-admin/{group_id}?"
+                                 f"orgonly=true&organizationId={organization_id}&supportedpurview=organizationId"
+            },
+            # data_format="grpc",
+        )
+
+    def get_group_members(self, group_id: str) -> list:
+        changed_members = []
+        members = self.identity_connector.list_memberships(group_id)
+        for member in members:
+            member_name = member.get("name")
+            member_info = self.identity_connector.get_membership(member_name)
+            roles = member_info.get("roles", [])
+            if len(roles) > 1:
+                for role in roles:
+                    if role.get("name") != "MEMBER":
+                        member_info["role"] = role.get("name")
+
+                member_info["role"] = "MEMBER"
+            changed_members.append(member_info)
+
+        return changed_members
